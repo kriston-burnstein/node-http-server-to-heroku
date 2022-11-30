af8b9082c2dbf815035471e81146a737a5548575
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-11-30 16:06:09 -0500

    Add comments to url_demo.js

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -4,30 +4,31 @@ const url = require('url');
 *  if you get an error saying, TypeError: URL is not a constructor, 
 *  comment line 1, and uncomment line 2 */
 
-const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active');
+const myUrl = new URL('http://mywebsite.com:8000/hello.html?id=100&status=active');
 
-// Serialized URL
-console.log(myUrl.href);
-console.log(myUrl.toString());
+// Serialized URL (basically the straight up url)
+console.log(myUrl.href); // returns entire url
+console.log(myUrl.toString()); // also returns entire url
 
 // Host (root domain)
-console.log(myUrl.host);
+console.log(myUrl.host); // mywebsite.com:8000 (will also give yout he port)
 
 // Hostname (does not get port)
-console.log(myUrl.hostname);
+console.log(myUrl.hostname); //mywebsite.com
 
 // Pathname
-console.log(myUrl.pathname);
+console.log(myUrl.pathname); // /hello.html (just the actual file)
 
-// Serialized query
-console.log(myUrl.search);
+// Serialized query (basically the straight up query params)
+console.log(myUrl.search); // ?id=100&status=active
 
 // Params object
-console.log(myUrl.searchParams);
+console.log(myUrl.searchParams); // URLSearchParams { 'id' => '100', 'status' => 'active' }
 
 // Add param
 myUrl.searchParams.append('abc', '123');
-console.log(myUrl.searchParams);
+console.log(myUrl.searchParams); // URLSearchParams { 'id' => '100', 'status' => 'active', 'abc' => '123' }
 
 // Loop through params
 myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`));
+// returns each key value pair
